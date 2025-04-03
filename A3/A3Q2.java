5f086290be897f4e9c908394c5e168d29dbf34a8
Jessica <jessicaxiee@outlook.com>
2025-04-03 15:15:58 -0400

    A3 starter code

---
+++ A3/A3Q2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class A3Q2 {
+
+    public static String[] time_pass(String[][] itinerary, String[] cities) {
+        String[] answer = new String[cities.length];
+        return answer;
+    }
+}
---
+++ A3/A3Q3.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.lang.*;
+
+public class A3Q3 {
+    public static double arduino(double[][] locations) {
+        double wire = 0.0;
+        return wire;
+    }
+}
